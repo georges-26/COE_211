1f91ecb7357a0b3ff047caac1de1a8498c3ddfd4
Georges Barhouche <georgebarhouch@gmail.com>
2022-02-22 18:03:21 +0200

    HelloWorld.java

---
+++ StaffManagement/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+     String FirstName;
+        String LastName;
+        int Age;
+        double MonthlySalary;
+    
+	Employee(String first,String last,int age,double sal){
+		FirstName = first;
+		LastName = last;
+		Age = age;
+		MonthlySalary = sal;
+	}
+}
